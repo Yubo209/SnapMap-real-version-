d8029a974428f8cec89d0753cdc7a631a8369189
Yubo209 <duyubo9@gmail.com>
2025-10-21 17:47:01 -0500

    nothing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
+
 import { Routes, Route } from 'react-router-dom';
-import ViewportScaler from './components/ViewportScaler';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import PrivateRoute from './utils/PrivateRoute';
@@ -10,29 +9,22 @@ import Home from './pages/Home';
 
 export default function App() {
   return (
-    <ViewportScaler
-      designWidth={1024}   
-      designHeight={680}   
-      minScale={0.72}      
-      maxScale={1}         
-      mobileOnly={true}    
-    >
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<MyProfile />} />
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        />
-      </Routes>
-    </ViewportScaler>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/profile" element={<MyProfile />} />
+      <Route
+        path="/dashboard"
+        element={
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        }
+      />
+    </Routes>
   );
 }
+
 
 
